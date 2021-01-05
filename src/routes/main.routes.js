d8bad06f700314dc7510f009e43b19f48a26f394
Robert Andrade <robert_andrade@icloud.com>
2021-01-05 16:51:52 -0300

    Alteracao back

---
+++ src/routes/main.routes.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        res.json ({ error: false, message: 'Hello Porra'});
+    }catch (err) {
+        res.json ({ error: true, message: err.message});
+    }
+})
+
+
+module.exports = router;

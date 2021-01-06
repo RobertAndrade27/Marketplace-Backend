f057c603ec0f3b4b7ddd79a77d293591a9b21619
Robert Andrade <robert_andrade@icloud.com>
2021-01-06 16:44:23 -0300

    Conexao com banco de dados e frontend

---
+++ src/data/addPetshopAndProducts.js
@@ -0,0 +1,24 @@
+const Petshop = require ('../models/petshop')
+const Product = require ('../models/product')
+const petshops = require ('./petfood.json')
+
+require('../database')
+
+const addPetshopsAndProducts = async () => {
+    try {
+
+        for (let petshop of petshops) {
+            const newPetshop = await new Petshop(petshop).save();
+            await Product.insertMany(
+                petshop.produtos.map(p => ({ ... p, petshop_id: newPetshop._id}))
+            );
+        }
+
+        console.log('final do Script')
+
+    }catch (err) {
+        console.log(err.message);
+    }
+};
+
+addPetshopsAndProducts ();
